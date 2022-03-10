d2a1ba874021bd0ecaa2e1d168dbdc61f58dfd56
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-03-10 11:40:32 -0500

    Example mbedtls chunk posting program
    See [README.md](README.md) for instructions.

--- programs/ssl/ssl_client1.c
+++ programs/ssl/ssl_client1.c
@@ -59,9 +59,21 @@ int main( void )
 
 #include <string.h>
 
-#define SERVER_PORT "4433"
-#define SERVER_NAME "localhost"
-#define GET_REQUEST "GET / HTTP/1.0\r\n\r\n"
+#define SERVER_PORT "443"
+#define SERVER_NAME "chunks.memfault.com"
+
+// format string for building the HTTP header
+#define POST_REQUEST \
+    "POST /api/v0/chunks/TESTSERIAL HTTP/1.1\r\n" \
+    "Host:chunks.memfault.com\r\n" \
+    "User-Agent: MemfaultSDK/0.4.2\r\n" \
+    "Memfault-Project-Key:%s\r\n" \
+    "Content-Type:application/octet-stream\r\n" \
+    "Content-Length:%zu\r\n\r\n" \
+
+// set the memfault PEM cert in this variable
+#include "3rdparty/memfault-firmware-sdk/components/include/memfault/http/root_certs.h"
+const char memfault_cert[] = MEMFAULT_ROOT_CERTS_DIGICERT_GLOBAL_ROOT_CA;
 
 #define DEBUG_LEVEL 1
 
@@ -124,8 +136,42 @@ int main( void )
     mbedtls_printf( "  . Loading the CA root certificate ..." );
     fflush( stdout );
 
-    ret = mbedtls_x509_crt_parse( &cacert, (const unsigned char *) mbedtls_test_cas_pem,
-                          mbedtls_test_cas_pem_len );
+    // default cert from mbedtls example
+    // ret = mbedtls_x509_crt_parse( &cacert, (const unsigned char *) mbedtls_test_cas_pem,
+    //                       mbedtls_test_cas_pem_len );
+
+    /*
+    // Add the amazon cert for httpbin.org
+    const char amazon_pem[] = \
+        "-----BEGIN CERTIFICATE-----\r\n" \
+        "MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\r\n" \
+        "ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\r\n" \
+        "b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\r\n" \
+        "MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\r\n" \
+        "b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\r\n" \
+        "ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\r\n" \
+        "9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\r\n" \
+        "IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\r\n" \
+        "VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\r\n" \
+        "93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\r\n" \
+        "jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\r\n" \
+        "AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\r\n" \
+        "A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\r\n" \
+        "U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\r\n" \
+        "N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\r\n" \
+        "o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\r\n" \
+        "5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\r\n" \
+        "rqXRfboQnoZsG4q5WTP468SQvvG5\r\n" \
+        "-----END CERTIFICATE-----\r\n" \
+        ;
+    ret = mbedtls_x509_crt_parse( &cacert, (const unsigned char *) amazon_pem,
+                          sizeof(amazon_pem) );
+                          */
+
+    // Memfault cert
+    ret = mbedtls_x509_crt_parse( &cacert, (const unsigned char *) memfault_cert,
+                          sizeof(memfault_cert) );
+
     if( ret < 0 )
     {
         mbedtls_printf( " failed\n  !  mbedtls_x509_crt_parse returned -0x%x\n\n", (unsigned int) -ret );
@@ -166,9 +212,9 @@ int main( void )
 
     mbedtls_printf( " ok\n" );
 
-    /* OPTIONAL is not optimal for security,
-     * but makes interop easier in this simplified example */
-    mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_OPTIONAL );
+    mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_REQUIRED );
+    // mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_OPTIONAL );
+
     mbedtls_ssl_conf_ca_chain( &conf, &cacert, NULL );
     mbedtls_ssl_conf_rng( &conf, mbedtls_ctr_drbg_random, &ctr_drbg );
     mbedtls_ssl_conf_dbg( &conf, my_debug, stdout );
@@ -228,14 +274,48 @@ int main( void )
         mbedtls_printf( " ok\n" );
 
     /*
-     * 3. Write the GET request
+     * 3. Write the POST request
      */
     mbedtls_printf( "  > Write to server:" );
     fflush( stdout );
 
-    len = sprintf( (char *) buf, GET_REQUEST );
+      const char *apikey = getenv("MEMFAULT_HTTPTEST_API_KEY");
+  if (!apikey) {
+      ret = 1;
+    mbedtls_printf("ERROR: set MEMFAULT_HTTPTEST_API_KEY\n\n");
+    goto exit;
+  }
 
-    while( ( ret = mbedtls_ssl_write( &ssl, buf, len ) ) <= 0 )
+    // example chunk data
+  const unsigned char chunk[] = {
+    0x08, 0x02, 0xa7, 0x02, 0x01, 0x03, 0x01, 0x07, 0x6a, 0x54, 0x45, 0x53, 0x54, 0x53, 0x45,
+    0x52, 0x49, 0x41, 0x4c, 0x0a, 0x6d, 0x74, 0x65, 0x73, 0x74, 0x2d, 0x73, 0x6f, 0x66, 0x74,
+    0x77, 0x61, 0x72, 0x65, 0x09, 0x6a, 0x31, 0x2e, 0x30, 0x2e, 0x30, 0x2d, 0x74, 0x65, 0x73,
+    0x74, 0x06, 0x6d, 0x74, 0x65, 0x73, 0x74, 0x2d, 0x68, 0x61, 0x72, 0x64, 0x77, 0x61, 0x72,
+    0x65, 0x04, 0xa1, 0x01, 0xa1, 0x72, 0x63, 0x68, 0x75, 0x6e, 0x6b, 0x5f, 0x74, 0x65, 0x73,
+    0x74, 0x5f, 0x73, 0x75, 0x63, 0x63, 0x65, 0x73, 0x73, 0x01, 0x31, 0xe4};
+
+    // format the request
+    unsigned char sendbuf[2048];
+    len = sprintf( (char *)sendbuf, POST_REQUEST, apikey, sizeof(chunk) );
+
+    mbedtls_printf("\nHeader: \n%s\n", sendbuf);
+
+    // send the header
+    while( ( ret = mbedtls_ssl_write( &ssl, sendbuf, len ) ) <= 0 )
+    {
+        if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE )
+        {
+            mbedtls_printf( "\n failed\n  ! mbedtls_ssl_write returned %d\n\n", ret );
+            goto exit;
+        }
+    }
+
+    len = ret;
+    mbedtls_printf( "\n %d header bytes written\n\n%s", len, (char *) buf );
+
+    // send the payload
+    while( ( ret = mbedtls_ssl_write( &ssl, chunk, sizeof(chunk) ) ) <= 0 )
     {
         if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE )
         {
@@ -245,7 +325,7 @@ int main( void )
     }
 
     len = ret;
-    mbedtls_printf( " %d bytes written\n\n%s", len, (char *) buf );
+    mbedtls_printf( " %d payload bytes written\n\n%s", len, (char *) buf );
 
     /*
      * 7. Read the HTTP response
@@ -278,7 +358,7 @@ int main( void )
         }
 
         len = ret;
-        mbedtls_printf( " %d bytes read\n\n%s", len, (char *) buf );
+        mbedtls_printf( " %d bytes read\n\n%s\n\n", len, (char *) buf );
     }
     while( 1 );
 
